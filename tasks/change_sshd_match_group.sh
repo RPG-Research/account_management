cb6c4f55865f2b5217605a49cc5a6f75113636d6
Niklas Anderson <niklas@rpgresearch.com>
2021-01-02 22:02:32 -0800

    working awk commands!

--- tasks/change_sshd_match_group.sh
+++ tasks/change_sshd_match_group.sh
@@ -13,10 +13,43 @@ changeFlag=0
 echo "Group is: "$PT_group
 echo "sshd_config is:" $PT_sshdconfiglocation
 
-function apply_changes () {
-        echo -e "We're changing:\n$sshdSectionOrig\n"
-        echo -e "To:\n$sshdSectionNew"
-	sed -i "s|$(echo -e "$1" | sed -e $'N;s/[\\n\r])/$(echo -e "$s2" | sed -e $'N;s/[\\n\r]//g' -e 's/       /\\n\\t/g')/g' -e 's/       /\\n\\t/g')|$2|g" $3	
+passwordauth () {
+	# Presently lacking code for switching password auth on yet ...
+	# $1 is expected to be the parameter, $2 is expected to be the group name, $3 is expected to be the sshd_config location 
+	if [[ $(expr match $1 '^\(n\|N\)') ]]; then
+		sshdSectionOrig=$(grep -Pzo "(Match Group $PT_group)(\n(\t|\s+)\w.+)*" $PT_sshdconfiglocation)
+		grepForPubkeyEntry=$(echo "$sshdSectionOrig" | grep PubkeyAuthentication | sed -e 's/\t//g')
+		echo "passwordauth parameter is 'No'..."
+		if [[ $grepForPasswordEntry == "PasswordAuthentication no" ]]; then
+			echo "Entry for PasswordAuthentication is already present as 'no'"
+			echo "$sshdSectionOrig"
+		elif [[ $grepForPubkeyEntry == "PasswordAuthentication no" ]]; then
+			echo "PubkeyAuthentication set to no, changing...."
+			awk -i inplace -v group="$2" -v addedText="\tPasswordAuthentication no" -v origText="\tPasswordAuthentication yes" '/Match Group '$group'/,/^$/ {gsub(origText, addedText)};{print; 1}' $3
+		elif [[ -z $grepForPasswordEntry ]]; then
+			echo "PasswordAuthentication not found, adding this entry..."
+			awk -i inplace -v group="$2" -v addedText="\tPasswordAuthentication no" '/Match Group '$group'/,/^$/ {gsub(/^$/, addedText)};{print; 1}' $3
+		fi
+	fi
+}
+
+pubkeyauth () {
+	# Presently lacking code for switching pubkeyauth off...
+	if [[ $(expr match $PT_pubkeyauth '^\(y\|Y\)') ]]; then
+		sshdSectionOrig=$(grep -Pzo "(Match Group $2)(\n(\t|\s+)\w.+)*" $3)
+		echo "pubkeyauth parameter is true..."
+		grepForPubkeyEntry=$(echo "$sshdSectionOrig" | grep PubkeyAuthentication | sed -e 's/\t//g')
+		if [[ $grepForPubkeyEntry == "PubkeyAuthentication yes" ]]; then
+			echo "Entry for PubkeyAuthentication is already present as 'yes'"
+			echo "$sshdSectionOrig"
+		elif [[ $grepForPubkeyEntry == "PubkeyAuthentication no" ]]; then
+			echo "PubkeyAuthentication set to no, changing...."
+			awk -i inplace -v group="$2" -v addedText="\tPubKeyAuthentication yes" -v origText="\tPubKeyAuthentication no" '/Match Group '$group'/,/^$/ {gsub(origText, addedText)};{print; 1}' $3
+		elif [[ -z $grepForPubkeyEntry ]]; then
+			echo "PubkeyAuthentication not found, adding..."
+			awk -i inplace -v group="$2" -v addedText="\tPubkeyAuthentication yes" '/Match Group '$group'/,/^$/ {gsub(/^$/, addedText)};{print; 1}' $3
+		fi
+	fi
 }
 
 if [[ $PT_group ]]; then
@@ -33,41 +66,11 @@ if [[ $PT_group ]]; then
 
 	echo $sshdSectionOrig
 
-	if [[ $(expr match $PT_pubkeyauth '^\(y\|Y\)') ]]; then
-		sshdSectionOrig=$(grep -Pzo "(Match Group $PT_group)(\n(\t|\s+)\w.+)*" $PT_sshdconfiglocation)
-		echo "pubkeyauth parameter is true..."
-		grepForPubkeyEntry=$(echo "$sshdSectionOrig" | grep PubkeyAuthentication | sed -e 's/\t//g')
-		if [[ $grepForPubkeyEntry == "PubkeyAuthentication yes" ]]; then
-			echo "Entry for PubkeyAuthentication is already present as 'yes'"
-			echo "$sshdSectionOrig"	
-		elif [[ $grepForPubkeyEntry == "PubkeyAuthentication no" ]]; then
-			echo "PubkeyAuthentication set to no, changing...."
-			sshdSectionNew=$(echo $sshdSectionOrig | sed -e 's/PubkeyAuthentication no/PubkeyAuthentication yes/g')
-                        apply_changes '"$sshdSectionOrig"' '"$sshdSectionNew"' "$PT_sshdconfiglocation"
-		elif [[ -z $grepForPubkeyEntry ]]; then
-			echo "PubkeyAuthentication not found, adding..."
-			#For the line below, escapes are required for \ so sed does not break
-			sshdSectionNew=$(echo -e "$sshdSectionOrig"'\\n\\tPubkeyAuthentication yes' | sed -e $'N;s/[\\n\r]//g' -e 's/   /\\n\\t/g')
-                        apply_changes "$sshdSectionOrig" "$sshdSectionNew" "$PT_sshdconfiglocation"
-		fi 
+	if [[ $(expr match $PT_pubkeyauth '^\(\(n\|N\)\|\(y\|Y\)\)') ]]; then
+		pubkeyauth "$PT_pubkeyauth" "$PT_group" "$PT_sshdconfiglocation"
 	fi
-	if [[ $(expr match $PT_passwordauth '^\(n\|N\)') ]]; then
-		sshdSectionOrig=$(grep -Pzo "(Match Group $PT_group)(\n(\t|\s+)\w.+)*" $PT_sshdconfiglocation)
-		echo "passwordauth parameter is true..."
-		grepForPasswordEntry=$(echo "$sshdSectionOrig" | grep PasswordAuthentication | sed -e 's/\t//g')
-		if [[ $grepForPasswordEntry == "PasswordAuthentication no" ]]; then
-			echo "Entry for PasswordAuthentication is already present as 'no'"
-			echo "$sshdSectionOrig"
-		elif [[ $grepForPubkeyEntry == "PasswordAuthentication no" ]]; then
-			echo "PubkeyAuthentication set to no, changing...."
-			sshdSectionNew=$(echo $sshdSectionOrig | sed -e 's/PasswordAuthentication no/PasswordAuthentication yes/g')
-			apply_changes "$sshdSectionOrig" "$sshdSectionNew" "$PT_sshdconfiglocation"
-		elif [[ -z $grepForPasswordEntry ]]; then
-			echo "PasswordAuthentication not found, adding..."
-			#For the line below, escapes are required for \ so sed does not break
-			sshdSectionNew=$(echo -e "$sshdSectionOrig"'\\n\\tPasswordAuthentication no' | sed -e $'N;s/[\\n\r]/\\n/g' -e 's/       /\\n\\t/g')
-			apply_changes "$sshdSectionOrig" "$sshdSectionNew" "$PT_sshdconfiglocation"
-		fi
+	if [[ $(expr match $PT_passwordauth '^\(\(n\|N\)\|\(y\|Y\)\)') ]]; then
+		passwordauth "$PT_passwordauth" "$PT_group" "$PT_sshdconfiglocation"
 	fi
 elif [[ -z $PT_group ]]; then
 	echo "No group provided."
